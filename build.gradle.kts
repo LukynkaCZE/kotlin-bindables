85786e244853183c57925f5dca93bfa674bc7a56
LukynkaCZE <jiraklukas058@seznam.cz>
2024-07-21 18:51:06 +0200

    testing + publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.9.23"
 }
 
 group = "cz.lukynka"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -11,12 +12,51 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation(kotlin("reflect"))
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
+    val javaToolchains = project.extensions.getByType<JavaToolchainService>()
+    javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    })
 }
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "21"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "21"
+    }
+    compileJava {
+        targetCompatibility = "21"
+    }
+    compileTestJava {
+        targetCompatibility = "21"
+    }
+}
+
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://mvn.devos.one/releases")
+            credentials {
+                username = System.getenv()["MAVEN_USER"]
+                password = System.getenv()["MAVEN_PASS"]
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("maven") {
+            groupId = "cz.lukynka"
+            artifactId = "kotlin-bindables"
+            version = version
+            from(components["java"])
+        }
+    }
 }
